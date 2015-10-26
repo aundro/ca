ab566c972bb19e7c653d3151cae501d0acdee606
Arnaud Diederen <arnaud@hex-rays.com>
2015-10-26 20:07:44 +0100

    Support for recursive @include{}, allowing split of data in subfiles

--- tools/inject.py
+++ tools/inject.py
@@ -8,21 +8,23 @@ parser.add_argument("-i", "--input")
 parser.add_argument("-o", "--output")
 args = parser.parse_args()
 
+regex = re.compile("(.*)@include{([^}]*)}(.*)")
 
-def process(inpath, outpath):
-    regex = re.compile("(.*)@include{([^}]*)}(.*)")
+def process(inpath, outf):
     with open(inpath, "rb") as inf:
-        with open(outpath, "wb") as outf:
-            for line in inf:
-                match = regex.match(line)
-                if match:
-                    with open(
-                        os.path.join(os.path.dirname(inpath),
-                                     match.group(2))) as dataf:
-                        data = "".join(dataf.readlines())
-                    line = match.group(1) + data + match.group(3)
+        for line in inf:
+            match = regex.match(line)
+            if match:
+                outf.write(match.group(1))
+                datapath = os.path.abspath(
+                    os.path.join(os.path.dirname(inpath),
+                                 match.group(2)))
+                process(datapath, outf)
+                outf.write(match.group(3))
+            else:
                 outf.write(line)
 
 
 if __name__ == "__main__":
-    process(os.path.abspath(args.input), os.path.abspath(args.output))
+    with open(os.path.abspath(args.output), "wb") as outf:
+        process(os.path.abspath(args.input), outf)
